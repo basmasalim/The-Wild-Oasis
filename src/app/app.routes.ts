771ef065fe1fa02a623d92f24eb0f69205270264
basmasalim <basmasalim030@gmail.com>
2025-08-11 18:52:57 +0300

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './core/layouts/dashboard/auth-layout/auth-layout';
 
 export const routes: Routes = [
   // ?====================> Auth Layout<=================
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
-    path: 'login',
-    loadComponent: () =>
-      import('./auth/components/login/login').then((m) => m.Login),
+    path: 'auth',
+    component: AuthLayout,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./auth/components/login/login').then((m) => m.Login),
+      },
+    ],
   },
 ];
