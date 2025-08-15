a002eabdf087dcc1238ef6ffe4d090744de02749
basmasalim <basmasalim030@gmail.com>
2025-08-15 13:37:10 +0300

    delete booking

--- src/app/features/pages/dashboard/dashboard-bookings/dashboard-bookings.ts
+++ src/app/features/pages/dashboard/dashboard-bookings/dashboard-bookings.ts
@@ -9,54 +9,53 @@ import { Component, inject, OnInit, signal } from '@angular/core';
   selector: 'app-dashboard-bookings',
   imports: [],
   templateUrl: './dashboard-bookings.html',
-  styleUrl: './dashboard-bookings.scss'
+  styleUrl: './dashboard-bookings.scss',
 })
 export class DashboardBookings implements OnInit {
-  private readonly bookings = inject(Bookings);
-  private readonly notifications = inject(Notifications);
-  bookingsList = signal<IBookings[]>([]);
+  // private readonly bookings = inject(Bookings);
+  // private readonly notifications = inject(Notifications);
+  // bookingsList = signal<IBookings[]>([]);
 
   ngOnInit() {
-    this.getAllBookings();
-    this.getSpecificBooking('28mtaFgKTJUseRqAoQOb');
+    // this.getAllBookings();
+    // this.getSpecificBooking('28mtaFgKTJUseRqAoQOb');
   }
 
-  getAllBookings() {
-    this.bookings.getBookings().subscribe({
-      next: (res) => {
-        this.bookingsList.set(res);
-        console.log('All Bookings:', this.bookingsList);
-      },
-      error: (error) => {
-        this.notifications.showError('Error fetching bookings', 'Please try again later.');
-        console.error('Error fetching bookings:', error);
-      }
-    })
-  }
+  // getAllBookings() {
+  //   this.bookings.getBookings().subscribe({
+  //     next: (res) => {
+  //       this.bookingsList.set(res);
+  //       console.log('All Bookings:', this.bookingsList);
+  //     },
+  //     error: (error) => {
+  //       this.notifications.showError('Error fetching bookings', 'Please try again later.');
+  //       console.error('Error fetching bookings:', error);
+  //     }
+  //   })
+  // }
 
-  getSpecificBooking(id: string) {
-    this.bookings.getBookingById(id).subscribe({
-      next: (res) => {
-        console.log('Booking Details:', res);
-      },
-      error: (error) => {
-        this.notifications.showError('Error fetching booking details', 'Please try again later.');
-        console.error('Error fetching booking details:', error);
-      }
-    });
-  }
+  // getSpecificBooking(id: string) {
+  //   this.bookings.getBookingById(id).subscribe({
+  //     next: (res) => {
+  //       console.log('Booking Details:', res);
+  //     },
+  //     error: (error) => {
+  //       this.notifications.showError('Error fetching booking details', 'Please try again later.');
+  //       console.error('Error fetching booking details:', error);
+  //     }
+  //   });
+  // }
 
-  deleteBooking(bookingId: string) {
-    this.bookings.deleteBooking(bookingId).subscribe({
-      next: () => {
-        // this.notifications.showSuccess('Booking deleted successfully', 'The booking has been removed.');
-        this.getAllBookings();
-      },
-      error: (error) => {
-        this.notifications.showError('Error deleting booking', 'Please try again later.');
-        console.error('Error deleting booking:', error);
-      }
-    });
-  }
-
+  // deleteBooking(bookingId: string) {
+  //   this.bookings.deleteBooking(bookingId).subscribe({
+  //     next: () => {
+  //       // this.notifications.showSuccess('Booking deleted successfully', 'The booking has been removed.');
+  //       this.getAllBookings();
+  //     },
+  //     error: (error) => {
+  //       this.notifications.showError('Error deleting booking', 'Please try again later.');
+  //       console.error('Error deleting booking:', error);
+  //     }
+  //   });
+  // }
 }
