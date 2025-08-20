b118544191161c43b0d98285c8f2c86c52a6fdb7
basmasalim <basmasalim030@gmail.com>
2025-08-20 18:34:00 +0300

    update DashboardBookings and delet pipe

--- src/app/core/pipe/booking-status-filter/bookingstatus-filter-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BookingstatusFilterPipe } from './bookingstatus-filter-pipe';
-
-describe('BookingstatusFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BookingstatusFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
