e238b216eb372ee7179b4686dcb94c2bcc505328
basmasalim <basmasalim030@gmail.com>
2025-08-24 20:45:05 +0300

    Home Filter box

--- src/app/features/pages/dashboard/dashboard-home/dashboard-home.ts
+++ src/app/features/pages/dashboard/dashboard-home/dashboard-home.ts
@@ -1,29 +1,29 @@
 import { Home } from './../../../../core/enum/home.enum';
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { FormsModule } from '@angular/forms';
 import { DAYSOPTIONS_CONSTANTS } from '../../../../core/constants/daysOptions.constants';
 import { RecentBox } from '../../../../shared/components/ui/recent-box/recent-box';
-import { Starts_CONSTANTS } from '../../../../core/constants/stats.constants';
-import { CircleChart } from "../../../../shared/components/business/circle-chart/circle-chart";
-import { LineChart } from "../../../../shared/components/business/line-chart/line-chart";
-import { TodayTable } from '../../../../shared/components/ui/today-table/today-table';
+import { STATS_BY_DAYS_CONSTANTS } from '../../../../core/constants/stats.constants';
+import { CircleChart } from '../../../../shared/components/business/circle-chart/circle-chart';
+import { LineChart } from '../../../../shared/components/business/line-chart/line-chart';
 @Component({
   selector: 'app-dashboard-home',
-  imports: [FormsModule, SelectButtonModule, RecentBox, CircleChart, LineChart, TodayTable],
+  imports: [FormsModule, SelectButtonModule, RecentBox, CircleChart, LineChart],
   templateUrl: './dashboard-home.html',
   styleUrl: './dashboard-home.scss',
 })
 export class DashboardHome {
-  first = 0;
-
   daysOptions = DAYSOPTIONS_CONSTANTS;
-  stats = signal(Starts_CONSTANTS);
 
-  filteredStatus: Home | '' = this.daysOptions[0].value;
+  filteredStatus = signal<Home>(this.daysOptions[0].value);
 
-  applyFilter(status: Home | ''): void {
-    this.filteredStatus = status;
-    this.first = 0;
+  // computed stats based on selected filter
+  filteredStats = computed(
+    () => STATS_BY_DAYS_CONSTANTS[this.filteredStatus()]
+  );
+
+  applyFilter(status: Home): void {
+    this.filteredStatus.set(status);
   }
 }
